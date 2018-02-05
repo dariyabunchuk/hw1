4c4a064fe992097a094d9efd92055d2d249fe854
Dariya Bunchuk <dariya.bunchuk@kellogg.northwestern.edu>
2018-02-05 14:05:30 -0600

    updates

--- hw1.js
+++ hw1.js
@@ -7,14 +7,14 @@
 // HOMEWORK 1
 
 // Given the following JavaScript object that represents weather data,
-// write a weather summary out to the screen, when this code is executed. 
+// write a weather summary out to the screen, when this code is executed.
 // Something like this:
 
 // Currently it is 67 degrees and sunny. Tomorrow it will be 65 and Mostly Cloudy.
 // The next day it we will see Partly Cloudy skies, with a temperature of 70.
 
 let weatherData = {
-  current: { 
+  current: {
     temperature: 67,
     conditions: "Sunny"
   },
@@ -23,5 +23,8 @@ let weatherData = {
     { temperature: 70, conditions: "Partly Cloudy" }
   ]
 }
-
-console.log("Do your homework!");
+let temperature=["65","70"];
+let forecast=["Mostly Cloudy","Partly Cloudy"];
+console.log("Currently it is " + weatherData.current.temperature+" degrees and sunny.");
+console.log("Tomorrow it will be " + temperature[0] + " and " + forecast[0] +".");
+console.log("The next day we will see " + forecast[1] + " skies, with a temperature of " + temperature[1] +".");
